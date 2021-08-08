3f2e8c968a66d3ad9a5cd4c2a435019cacac2880
Anwesh Mahapatra <anweshmahapatra123@yahoo.com>
2021-08-08 13:56:35 +0530

    two

--- domainAdmins_enum.ps1
+++ domainAdmins_enum.ps1
@@ -1,5 +1,5 @@
 $domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-$PDC = ($domainObj.PdcRoleOwner).
+$PDC = ($domainObj.PdcRoleOwner).Name
 $SearchString = "LDAP://"
 $SearchString += $PDC + "/"
 $DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
@@ -7,14 +7,9 @@ $SearchString += $DistinguishedName
 $Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
 $objDomain = New-Object System.DirectoryServices.DirectoryEntry
 $Searcher.SearchRoot = $objDomain
-$Searcher.filter="samAccountName=domain" #Change this to $searcher.filter="name=Jeff_admin"
+$Searcher.filter="(name=Domain Admins)"
 $Result = $Searcher.FindAll()
 Foreach($obj in $Result)
 {
-    Foreach($prop in $obj.Properties)
-    {
-        $prop
-    }
-}
-    Write-Host "------------------------"
+    $obj.Properties.member
 }
